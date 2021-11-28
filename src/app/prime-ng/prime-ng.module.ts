ec276f080a1357e124eb9923b1f761368d4761a6
jhoanrl93 <jrativa@bancolombia.com.co>
2021-11-27 22:11:07 -0500

    Add new feature

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    CardModule,
+    ButtonModule
+  ]
+})
+export class PrimeNgModule { }
